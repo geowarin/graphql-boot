7519983cd4855f57f21fb5bdd7f8c9b6db0c4625
geowarin <geowarin@gmail.com>
2017-11-11 13:16:10 +0100

    upgrade graphql-java and auto yarn start

--- src/main/kotlin/com/example/graphqlboot/GraphqlBootApplication.kt
+++ src/main/kotlin/com/example/graphqlboot/GraphqlBootApplication.kt
@@ -1,11 +1,29 @@
 package com.example.graphqlboot
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.zeroturnaround.exec.ProcessExecutor
+import org.zeroturnaround.exec.StartedProcess
+import org.zeroturnaround.exec.stream.slf4j.Slf4jStream
+import java.io.File
 
 @SpringBootApplication
 class GraphqlBootApplication
 
-fun main(args: Array<String>) {
-    SpringApplication.run(GraphqlBootApplication::class.java, *args)
+fun yarnStart(): StartedProcess {
+  return ProcessExecutor()
+    .command("yarn", "start")
+    .directory(File("./src/main/ts"))
+    .redirectOutput(Slf4jStream.of(LoggerFactory.getLogger("yarn")).asInfo())
+    .redirectError(Slf4jStream.of(LoggerFactory.getLogger("yarn")).asError()).start()
 }
+
+fun main(args: Array<String>) {
+  if (!isRestart()) {
+    yarnStart()
+  }
+  SpringApplication.run(GraphqlBootApplication::class.java, *args)
+}
+
+private fun isRestart() = Thread.currentThread().name == "restartedMain"
